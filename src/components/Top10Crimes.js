ab8530a361edbf33fd23e7972a70c43a82e0727f
adriane26 <adrianepurdy@yahoo.com>
2017-09-22 15:12:05 -0700

    added react-fetch

--- src/components/Top10Crimes.js
+++ src/components/Top10Crimes.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Fetch from 'react-fetch';
 import '../App.css';
 
 class Top10Crimes extends Component {
@@ -6,9 +7,19 @@ class Top10Crimes extends Component {
     return (
     	<div>
     	   <h1>10 Most Common Crimes</h1>
+           <Fetch url="https://onedrive.live.com/?authkey=%21ANgwlxZYDmkeuKo&v=TextFileEditor&id=1121E4DCC138F1E0%21109&cid=1121E4DCC138F1E0&parId=root" options="no-cors">
+               <MostCommon />
+           </Fetch>
     	</div>
     );
   }
+}
+
+class MostCommon extends Component {
+    render() {
+        console.log(this.props);
+        return <div/>
+    };
 }
 
 export default Top10Crimes;
