b868e86b78bc1bbc99a38c74e2b5cef7a1b68625
adriane26 <adrianepurdy@yahoo.com>
2017-09-22 13:28:32 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Layout from "./components/Layout";
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Tacoma Crime 2014 - 2016</h2>
+        </div>
+        <Layout />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Layout.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Layout extends Component {
+  render() {
+    return (
+    	<div>
+    		<div>
+    			<h1>Types of Crimes</h1>
+    		</div>
+    		<div>
+    			<h1>10 Most Common Crimes</h1>
+    		</div>
+    		<div>
+    			<h1>10 Least Common Crimes</h1>
+    		</div>
+    	</div>
+    );
+  }
+}
+
+export default Layout;
